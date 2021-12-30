34d631f5ef72f0d019fdc1868b4e39690f720854
mcpeblocker <mcpeblocker@gmail.com>
2021-12-30 23:57:00 +0500

    remove dev logs

--- src/commands/help.ts
+++ src/commands/help.ts
@@ -2,7 +2,5 @@ import bot from "../core/bot";
 import { bold } from "../utils/format";
 
 bot.command("help", async (ctx) => {
-    const text = ctx.i18n.t('help');
-    console.log("AA");
-    await ctx.replyWithHTML(text);
+    await ctx.replyWithHTML(ctx.i18n.t('help'));
 });
